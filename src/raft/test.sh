79b7b258adc8b06f34b67ca54b3426e58db35e7f
NaroZeol <narozeol@gmail.com>
2024-10-15 00:52:25 +0800

    lab3-test: update test script
    add dstest script
    
    update test.sh

--- src/raft/test.sh
+++ src/raft/test.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # 初始化计数器
-count=0
+count=1
 
-# 持续运行 go test -run 3A，直到返回值为 1
+# 持续运行 go test，直到返回值为 1
 while true
 do
+    # 打印当前执行次数
+    echo "第 $count 次执行"
+
     # 执行测试并获取返回值
-    go test -run 3A
+    go test
     result=$?
 
     # 增加执行次数计数器
     count=$((count+1))
-
-    # 打印当前执行次数
-    echo "第 $count 次执行"
 
     # 如果返回值为 1，则退出循环
     if [ $result -eq 1 ]; then
