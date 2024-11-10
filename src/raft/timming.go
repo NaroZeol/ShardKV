78bc35a256b4ca2a8d7a49803ad2505abf2628f5
NaroZeol <narozeol@gmail.com>
2024-11-10 21:37:17 +0800

    Revert "lab3-fix: fix a wrong check condition"
    This reverts commit bd6b82b.
    
    bd6b82b is proved to make another problem.
    
    Actually, the former check condition is set to fix the problem that
    cause "Test: Figure 8(unreliable)" failed
    
    Leader should only commit entries created in it's currentTerm
    
    So we need to find a new solution for the problem bd6b82b tried to
    fix

--- src/raft/timming.go
+++ src/raft/timming.go
@@ -4,8 +4,8 @@ import "time"
 
 const (
 	TM_HeartBeatInterval time.Duration = 100 * time.Millisecond // heart beat interval
-	TM_ElectionTimeout   time.Duration = 200 * time.Millisecond // election timeout
-	TM_RandomWaitingTime time.Duration = 300 * time.Millisecond // randomization, wait for 0~TM_RandomWaitingTime
+	TM_ElectionTimeout   time.Duration = 300 * time.Millisecond // election timeout
+	TM_RandomWaitingTime time.Duration = 800 * time.Millisecond // randomization, wait for 0~TM_RandomWaitingTime
 	TM_CommitCheck       time.Duration = 10 * time.Millisecond  // check interval of commit
 	TM_ApplyCheck        time.Duration = 10 * time.Millisecond  //check interval of apply
 	MAX_RETRY_TIMES      int           = 3                      // Max times a server can try during sending a RPC
